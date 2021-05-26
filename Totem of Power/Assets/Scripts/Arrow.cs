23521b09dd2e0da6daa9773c638604cc2e31292f
Heath Hughes <heathwhughes@gmail.com>
2021-05-26 09:08:05 -0500

    Added intial code to gradually rotate on arrow click

--- Totem of Power/Assets/Scripts/Arrow.cs
+++ Totem of Power/Assets/Scripts/Arrow.cs
@@ -6,31 +6,24 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     // This will make it move 90 degrees per second
-    [SerializeField] private float speed = 90;
-    float yRotation = 0;
+    [SerializeField] float rotateSpeed = 5f;
     bool rotateLeft = false;
     bool rotateRight = false;
-   
-    float testSpeed = 0.1f;
+    float rotationAngle;
+    Transform Quad;
 
+    private void Start()
+    {
+        Quad = gameObject.transform.parent.Find("Quads");
+        rotationAngle = Quad.eulerAngles.y;
+    }
 
     private void Update()
     {
         if (rotateLeft || rotateRight)
         {
-            // RotateQuads();    
+            RotateQuads();    
         }
-
-        // Interpolates rotation between the rotations
-        // of from and to.
-        // (Choose from and to not to be the same as
-        // the object you attach this script to)
-
-        // This works!!
-        var Quads = gameObject.transform.parent.Find("Quads");
-        print("Quads rotation before: " + Quads.transform.rotation);
-        Quads.transform.rotation = Quaternion.Lerp(Quads.transform.rotation, Quaternion.Euler(0f, 90, 0f), Time.deltaTime * testSpeed);
-        print("Quads rotation after: " + Quads.transform.rotation);
     }
 
     private void OnMouseDown()
@@ -39,38 +32,23 @@ public class Arrow : MonoBehaviour
         {
             rotateLeft = true;
             rotateRight = false;
+            rotationAngle += 90;
+            print("rotationAngle: " + rotationAngle);
         }
         else
         {
             rotateRight = true;
             rotateLeft = false;
-            print("Didn't click Left Arrow");
+            rotationAngle -= 90;
+            print("rotationAngle: " + rotationAngle);
         }
     }
 
     private void RotateQuads()
     {
-        if (rotateRight)
-        {
-            speed *= -1;
-        }
-        else
-        {
-            speed = Mathf.Abs(speed);
-        }
-
-        var myParent = gameObject.transform.parent;
-
-        var Quads = myParent.Find("Quads");
-        print("Quads rotation before update: " + Quads.rotation.eulerAngles);
-
-
-        yRotation += Time.deltaTime * speed;
-
-        print("Y rotation to be applied: " + yRotation);
-        Quaternion newYRotation = Quaternion.Euler(0f, Quads.rotation.eulerAngles.y + yRotation, 0f);
-        // Quads.rotation = Quaternion.Euler(0f, Quads.rotation.eulerAngles.y + yRotation, 0f);
-        Quads.rotation = Quaternion.Lerp(Quads.rotation, newYRotation, Time.time * speed);
-        print("Quads rotation after update: " + Quads.rotation);
+        Quad.transform.rotation = Quaternion.Slerp(
+            Quaternion.Euler(Quad.transform.eulerAngles),
+            Quaternion.Euler(0f, rotationAngle, 0f),
+            Time.deltaTime * rotateSpeed);
     }
 }
